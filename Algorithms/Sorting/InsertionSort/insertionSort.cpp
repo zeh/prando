45cf91f2a586e653138fdd960aaa5f84f8d3db9d
kirti0808 <gupta.kirti0808@gmail.com>
2018-10-11 02:07:47 +0530

    Added Longest Increasing Sub-sequence Algorithm

--- Algorithms/Sorting/InsertionSort/insertionSort.cpp
+++ Algorithms/Sorting/InsertionSort/insertionSort.cpp
@@ -1,21 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+  
+void insertionSort(int arr[], int n) 
+{ 
+   int i, key, j; 
+   for (i = 1; i < n; i++) 
+   { 
+       key = arr[i]; 
+       j = i-1; 
 
-int main{
-	int n=20, i;
-	int arr[n];
-	for(i=0; i<n; i++){
-		cin>>arr[i];
-	}
-	for(i=1; i<n; i++){
-		for(int j=i-1; j>=0; j--){
-			while(arr[j]>arr[i]){
-				a[j+1]=a[j];
-				j=j-1	
-			}
-		a[j+1] = a[i]; 
-		}
-	}
-	return 0;
-
-}
+       while (j >= 0 && arr[j] > key) 
+       { 
+           arr[j+1] = arr[j]; 
+           j = j-1; 
+       } 
+       arr[j+1] = key; 
+   } 
+} 
+ 
+void printArray(int arr[], int n) 
+{ 
+   int i; 
+   for (i=0; i < n; i++) 
+       cout<<arr[i]<<" ";
+   cout<<"\n"; 
+} 
+  
+int main() 
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+      cin>>arr[i];
+    }
+    insertionSort(arr, n); 
+    printArray(arr, n); 
+    return 0; 
+} 

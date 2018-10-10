13c072bc256d53501615934ba3f0e4ec0efe6fec
sushma <sushmak@bsf.io>
2018-10-10 14:11:04 +0530

    Comment updated

--- Algorithms/Mathematical Algorithms/fibonacci.php
+++ Algorithms/Mathematical Algorithms/fibonacci.php
@@ -2,8 +2,9 @@
 /*
  * @Author: Sushma Kure
  * @ProblemStatement: Program for Fibonacci numbers in PHP.
- * a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. 
- * The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+ * It gives a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding 
+ * numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+ *
  */
 
 function fibonacci( $number ){ 

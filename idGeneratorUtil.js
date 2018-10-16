4bff06acd50458dc7f19922523e46ee94d988ae9
Norrapat Nimmanee <charge_n@hotmail.com>
2018-10-16 23:15:11 +0700

    Add Utility ID Generator

---
+++ idGeneratorUtil.js
@@ -0,0 +1,13 @@
+const Hashids = require('hashids');
+const hashIds = new Hashids();
+
+module.exports = {
+  /**
+   * Generate Unique ID by timestamp
+   * 
+   * @returns {String}
+   */
+  generateId: function () {
+    return hashIds.encode(new Date().getTime());
+  },
+};

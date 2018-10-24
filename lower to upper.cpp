9984996d19232af3362052b4ed18d94ca3ca8173
bicmfcs <freeyoung.geek@gmail.com>
2018-10-24 22:46:06 +0330

    Add files via upload

--- lower to upper.cpp
+++ lower to upper.cpp
@@ -1,23 +1,23 @@
-#include<iostream.h>
+#include <iostream>
 #include<conio.h>
-void main()
+using namespace std ;
+int main()
 {
-clrscr();
 char ch;
-cout<<“Enter any Alphabet:”;
+cout<<"Enter any Alphabet:";
 cin>>ch;
 
-if(ch>=’a’&&ch<=’z’)
+if(ch>='a'&&ch<='z')
 {
-cout<<“ntYou have entered a lowercase alphabet”;
+cout<<"\tYou have entered a lowercase alphabet";
 ch=ch-32;
-cout<<“nnThe uppercase alphabet is “<<ch;
+cout<<"\nThe uppercase alphabet is "<<ch;
 }
 else
 {
-cout<<“ntYou have entered an Uppercase alphabet”;
+cout<<"\tYou have entered an Uppercase alphabet";
 ch=ch+32;
-cout<<“nnThe lowercase alphabet is “<<ch;
+cout<<"\nThe lowercase alphabet is "<<ch;
 }
 getch();
 }

1c80a2c51c587315c9020afcb029db5ed166261f
Andres <aguilar_andres@protonmail.com>
2018-10-24 02:05:30 -0400

    Update first_come_first_serve.cpp

--- Algorithms/CPU Scheduling/first_come_first_serve.cpp
+++ Algorithms/CPU Scheduling/first_come_first_serve.cpp
@@ -1,3 +1,11 @@
+/*
+- Jobs are executed on first come, first serve basis.
+- It is a non-preemptive, pre-emptive scheduling algorithm.
+- Easy to understand and implement.
+- Its implementation is based on FIFO queue.
+- Poor in performance as average wait time is high.
+*/
+
 #include <iostream>
 
 int main() {

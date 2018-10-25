7bd612357c352b001def41fb817b505db245012b
SAURABH SHEKHAR <42865533+SaurabhShekhar161@users.noreply.github.com>
2018-10-25 20:46:34 +0530

    Update Swap.c

--- Swap.c
+++ Swap.c
@@ -7,9 +7,9 @@ int main(int argc, char *argv[ ]) {
    scanf("%d %d", &a, &b);
 
    printf("Before Swap: A = [%d], B = [%d]\n", a, b);
-   a = a^b;
-   b = a^b;
-   a = a^b;
+   a = a+b;
+   b = a-b;
+   a = a-b;
    printf("After Swap: A = [%d], B = [%d]\n", a, b);
 
    return 0;

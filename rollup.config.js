6d9791df2e976d11c4ad77e67fc6627245d75dd5
Zeh Fernando <zeh@work.co>
2018-11-12 19:23:05 -0500

    Build: fixed pure TS imports with an exported default

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,14 @@ import pkg from "./package.json";
 export default {
 	input: "src/Prando.ts",
 	output: [
-		{ name: "Prando", sourcemap: true, file: pkg.module, format: "es" },
-		{ name: "Prando", sourcemap: true, file: pkg.main, format: "umd" },
+		{ name: "prando", sourcemap: true, file: pkg.module, format: "es" },
+		{
+			name: "Prando",
+			sourcemap: true,
+			file: pkg.main,
+			format: "umd",
+			footer: "module.exports.default = module.exports; // Terrible injection just so it works regardless of how it's required\n"
+		}
 	],
 	plugins: [
 		typescript({

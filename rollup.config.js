638f8f5ba1396487eb9b581834788323d075e178
Zeh Fernando <zeh@work.co>
2018-12-14 16:47:37 -0500

    Build: remove sourcemaps and built definition files into /dist

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,10 @@ import pkg from "./package.json";
 export default {
 	input: "src/Prando.ts",
 	output: [
-		{ name: "prando", sourcemap: true, file: pkg.module, format: "es" },
+		{ name: "prando", sourcemap: false, file: pkg.module, format: "es" },
 		{
 			name: "Prando",
-			sourcemap: true,
+			sourcemap: false,
 			file: pkg.main,
 			format: "umd",
 			footer: "module.exports.default = module.exports; // Terrible injection just so it works regardless of how it's required\n"
